47516b169e3afd1ddb41197d6aafe7af4099dc04
Reilly Chase <rchase@rchase.com>
2019-09-30 10:06:06 -0400

    Update change-unifi-ssl-installer-scripts.sh

--- change-unifi-ssl-installer-scripts.sh
+++ change-unifi-ssl-installer-scripts.sh
@@ -10,6 +10,12 @@ done
 echo $EMAIL;
 echo $HOSTNAMEVAR;
 
+echo "Removing Apache2"
+apt-get remove apache2 -y
+
+echo "Installing NGINX"
+apt-get install nginx-light -y
+
 certbot --nginx --email $EMAIL --agree-tos --no-eff-email --domain $HOSTNAMEVAR --no-redirect
 wget https://raw.githubusercontent.com/HostiFi/unifi-lets-encrypt-ssl-updater/master/unifi-lets-encrypt-ssl-updater.sh
 chmod +x /root/unifi-lets-encrypt-ssl-importer.sh
@@ -21,5 +27,42 @@ echo "0 23 * * * /bin/bash /root/unifi-lets-encrypt-ssl-importer.sh -d $HOSTNAME
 crontab /root/letsencryptcron
 rm /root/letsencryptcron
 
-# Remove old script
+echo "Removing old SSL script"
 rm /root/unifi-ssl.sh
+
+echo "Creating firewall rules"
+iptables -t nat -I PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 8443
+iptables -A INPUT -i ens3 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 8080 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 8443 -j ACCEPT
+iptables -A INPUT -p udp --dport 3478 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 8843 -j ACCEPT
+iptables -A INPUT -j DROP
+iptables -A OUTPUT -o ens3 -j ACCEPT
+
+echo "Saving firewall rules"
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
+apt-get install iptables-persistent netfilter-persistent -y
+
+netfilter-persistent save
+
+echo "Configuring NGINX to forward HTTP to HTTPS"
+echo "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    server_name _;
+    return 301 https://\$host\$request_uri;
+}" > /etc/nginx/sites-available/redirect
+ln -s /etc/nginx/sites-available/redirect /etc/nginx/sites-enabled/
+rm /etc/nginx/sites-enabled/default
+
+echo "Restarting services"
+systemctl restart nginx
+systemctl restart unifi
+
+echo "Done!"
