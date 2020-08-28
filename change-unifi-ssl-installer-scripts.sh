08e0328e41f3abcfa7e23c316dfc200d201c22fa
Reilly Chase <rchase@rchase.com>
2020-08-28 12:40:52 -0400

    Multidomain update

--- change-unifi-ssl-installer-scripts.sh
+++ change-unifi-ssl-installer-scripts.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-while getopts e:d: option
-do
-case "${option}"
-in
-e) EMAIL=${OPTARG};;
-d) HOSTNAMEVAR=${OPTARG};;
-esac
+# parse domains to an array
+#!/bin/bash
+# parse domains to an array
+while getopts e:d: option; do
+  case $option in
+    "d") DOMAINS+=("$OPTARG");;
+    "e") EMAIL=${OPTARG};;
+  esac
 done
 echo $EMAIL;
-echo $HOSTNAMEVAR;
+for DOMAIN in "${DOMAINS[@]}"; do
+  stringprefix=" -d "
+  stringpostfix=" "
+  DOMAINSTR+=$stringprefix$DOMAIN$stringpostfix
+done
+echo $DOMAINSTR;
 
 apt-get autoremove -y
 modprobe ip_tables
@@ -25,13 +31,13 @@ apt-get update -y
 apt-get install python-certbot-nginx -t stretch-backports -y
 
 echo "Getting cert"
-wget https://raw.githubusercontent.com/HostiFi/unifi-lets-encrypt-ssl-updater/master/unifi-lets-encrypt-ssl-updater.sh
+wget https://raw.githubusercontent.com/HostiFi/unifi-lets-encrypt-ssl-updater-multidomain/master/unifi-lets-encrypt-ssl-updater-multidomain.sh -O unifi-lets-encrypt-ssl-updater-multidomain.sh
 chmod +x /root/unifi-lets-encrypt-ssl-updater.sh
-/bin/bash /root/unifi-lets-encrypt-ssl-updater.sh -d $HOSTNAMEVAR -e $EMAIL
+/bin/bash /root/unifi-lets-encrypt-ssl-updater-multidomain.sh $DOMAINSTR -e $EMAIL
 
 echo "Creating Let's Encrypt cron"
 crontab -l > /root/letsencryptcron
-echo "0 23 * * * /bin/bash /root/unifi-lets-encrypt-ssl-importer.sh -d $HOSTNAMEVAR" >> /root/letsencryptcron
+echo "0 23 * * * /bin/bash /root/unifi-lets-encrypt-ssl-importer.sh -d ${DOMAINS[0]}" >> /root/letsencryptcron
 crontab /root/letsencryptcron
 rm /root/letsencryptcron
 crontab -l > /root/certbotcron
